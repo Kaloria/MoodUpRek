5999d1205e7188ce67c1e57b006eadec54286b2c
Ewa Żebrowska <eze@macrix.pl>
2015-01-22 07:56:06 +0100

    Initial Commit

---
+++ MoodUpRek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoodUpRek
+//
+//  Created by Ewa Żebrowska on 22/01/15.
+//  Copyright (c) 2015 Ewa Żebrowska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoodUpRek/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoodUpRek
+//
+//  Created by Ewa Żebrowska on 22/01/15.
+//  Copyright (c) 2015 Ewa Żebrowska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
